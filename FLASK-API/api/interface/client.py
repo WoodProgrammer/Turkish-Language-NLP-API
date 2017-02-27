4928e3356e582392c497a5758c38520f48962c87
Wood Programmer <emirozbir@hotmail.com>
2017-02-27 23:44:03 +0200

    Cleaning added

--- FLASK-API/api/interface/client.py
+++ FLASK-API/api/interface/client.py
@@ -6,10 +6,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 import requests
 import json
 from flask_restful import reqparse
+
+from jinja2 import Template
+
 parser = reqparse.RequestParser()
 app=Flask(__name__)
 
@@ -24,6 +27,8 @@ def send():
         api_word=str(unicode(args['word'][0]))
         api_data=requests.get('http://localhost:5000/api/{}'.format(api_word.lower())).content
 
+                #SEND GET REQUEST TO THE SERVER.
+
         return render_template("enter_data.html",api_data=api_data)
 
     return render_template("enter_data.html")
@@ -37,11 +42,18 @@ def tokenize():
         args = parser.parse_args()
         api_word=str(unicode(args['paragraph'][0].lower()))
         api_data=requests.get('http://localhost:5000/tokenize/{}'.format(api_word)).content
+                #SEND GET REQUEST TO THE SERVER.
+        api_data2=json.loads(api_data)
+        word_template=[]
 
-        return render_template("paragraph.html",api_data=json.loads(api_data))
+        for i in range(2) :
+            word_template.append(api_data2['word'][i])
+
+
+
+        return render_template("paragraph_data.html",api_data = word_template)
 
     return render_template("paragraph.html")
-
 
 
 @app.route('/document-validator',methods=["GET","POST"])
